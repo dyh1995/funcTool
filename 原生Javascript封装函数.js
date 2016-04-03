130d0ac36d3bb724c2cdf543fe1befef5d8b9d20
dyh <dongyuhao1995@qq.com>
2016-04-03 12:37:09 +0800

    第一版
    js中一些常用的函数

---
+++ 原生Javascript封装函数.js
@@ -0,0 +1,351 @@
+/**
+ * 得到ajax对象
+ */
+function getajaxHttp() {
+    var xmlHttp;
+    try {
+        // Firefox, Opera 8.0+, Safari
+        xmlHttp = new XMLHttpRequest();
+    } catch (e) {
+        // Internet Explorer
+        try {
+            xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {
+                alert("您的浏览器不支持AJAX！");
+                return false;
+            }
+        }
+    }
+    return xmlHttp;
+}
+
+/**
+ * 发送ajaxGet请求
+ * @param url
+ */
+function ajaxGet(url){
+
+    var xmlHttp  = getajaxHttp();
+    xmlHttp.open("GET",url);
+    xmlHttp.send(null);
+    xmlHttp.onreadystatechange = function() {
+        if ((xmlHttp.readyState == 4) && (xmlHttp.status == 200)) {
+            alert('success');
+        } else {
+            alert('fail');
+        }
+    }
+}
+
+/**
+ * 发送ajaxPost请求
+ * @param url
+ * @param data
+ */
+function ajaxPost(url,data){
+
+    var xmlHttp  = getajaxHttp();
+    xmlHttp.open("POST",url);
+    xmlHttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+    xmlHttp.send(data);
+    xmlHttp.onreadystatechange = function() {
+        if ((xmlHttp.readyState == 4) && (xmlHttp.status == 200)) {
+            alert('success');
+        } else {
+            alert('fail');
+        }
+    }
+}
+
+/**
+ *
+ * @param {Object} el HTML Element
+ * @param {Object} type 事件类型
+ * @param {Object} fn 事件handler
+ */
+function addEvent(el, type, fn){
+    if(el.addEventListener){
+        el.addEventListener(type, fn, true);
+    }else if(el.attachEvent){
+        el.attachEvent('on'+type, el['e'+fn]);
+    }else{
+        el['e' + fn] = function(){
+            fn.call(el, window.event);
+        };
+    }
+}
+
+/**
+ * 删除事件
+ * @param el
+ * @param type
+ * @param fn
+ */
+function removeEvent(el, type, fn){
+    if(el.removeEventListener){
+        el.removeEventListener(type, fn, false);
+    }else if(el.detachEvent){
+        el.detachEvent('on' + type, el['e'+fn]);
+    }
+}
+
+/**
+ * 根据id获取元素
+ * @param el
+ * @returns {Element}
+ */
+function getEl(el){
+    return document.getElementById(el);
+}
+
+/**
+ * 判断是否为对象类型
+ * @param obj
+ * @returns {boolean}
+ */
+RegExp.isObject = function (obj) {
+    return (typeof obj == 'object') && obj.constructor == Object;
+};
+
+/**
+ * 验证一个字符串时候是email
+ * @param str
+ * @returns {boolean}
+ */
+RegExp.isEmail = function (str) {
+    var emailReg = /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)*\.[\w-]+$/i;
+    return emailReg.test(str);
+};
+
+/**
+ * 验证一个字符串是否是URL
+ * @param str
+ * @returns {Array|{index: number, input: string}}
+ */
+RegExp.isUrl = function (str) {
+    var patrn = /^http(s)?:\/\/[A-Za-z0-9]+\.[A-Za-z0-9]+[\/=\?%\-&_~`@[\]\:+!]*([^<>])*$/;
+    return patrn.exec(str);
+};
+
+/**
+ * 转换时间戳
+ * @param nS
+ * @returns {string}
+ */
+function getLocalTime(nS) {
+    return new Date(parseInt(nS) * 1000).toLocaleString().replace(/年|月/g, "-").replace(/日/g, " ");
+}
+
+/**
+ * 将文件大小转成合适的刻度
+ * @param nS
+ * @returns {string}
+ */
+function fomateFileSize(nS) {
+    if (nS < 1024) {
+        return (nS / 1).toFixed(1) + " B";
+    } else if (nS < Math.pow(1024, 2) && nS > 1024) {
+        return (nS / Math.pow(1024, 1)).toFixed(1) + " KB";
+    } else if (nS < Math.pow(1024, 3) && nS > 1024 ^ 2) {
+        return (nS / Math.pow(1024, 2)).toFixed(1) + " MB";
+    } else if (nS < Math.pow(1024, 4) && nS > 1024 ^ 3) {
+        return (nS / Math.pow(1024, 3)).toFixed(1) + " GB";
+    } else if (nS < Math.pow(1024, 5) && nS > 1024 ^ 4) {
+        return (nS / Math.pow(1024, 4)).toFixed(1) + " TB";
+    }
+}
+
+/**
+ * 获取窗口宽度
+ * @returns {*}
+ */
+function getWindowWidth() {
+    if ( window.innerWidth ) {
+        return window.innerWidth;
+    } else if ( document.documentElement && document.documentElement.clientWidth ) {
+        return document.documentElement.clientWidth;
+    } else if ( document.body && document.body.clientWidth){
+        return document.body.clientWidth;
+    }
+}
+
+/**
+ * 获取窗口高度
+ * @returns {*}
+ */
+function getWindowHeight() {
+    if ( window.innerHeight ) {
+        return window.innerHeight;
+    } else if ( document.documentElement && document.documentElement.clientHeight ) {
+        return document.documentElement.clientHeight;
+    } else if ( document.body && document.body.clientHeight){
+        return document.body.clientHeight;
+    }
+}
+/*
+ 网页可见区域宽： document.body.clientWidth
+ 网页可见区域高： document.body.clientHeight
+ 网页可见区域宽： document.body.offsetWidth (包括边线的宽)
+ 网页可见区域高： document.body.offsetHeight (包括边线的高)
+ 网页正文全文宽： document.body.scrollWidth
+ 网页正文全文高： document.body.scrollHeight
+ 网页被卷去的高： document.body.scrollTop
+ 网页被卷去的左： document.body.scrollLeft
+ 网页正文部分上： window.screenTop
+ 网页正文部分左： window.screenLeft
+ 屏幕分辨率的高： window.screen.height
+ 屏幕分辨率的宽： window.screen.width
+ 屏幕可用工作区高度： window.screen.availHeight
+ 屏幕可用工作区宽度： window.screen.availWidth
+ */
+
+/**
+ * 判断浏览器类型
+ */
+function judgeBrowser(){
+    if ((navigator.userAgent.indexOf('MSIE') >= 0)
+        && (navigator.userAgent.indexOf('Opera') < 0)){
+        alert("IE");
+    }else if (navigator.userAgent.indexOf('Firefox') >= 0){
+        alert('你是使用Firefox')
+    }else if (navigator.userAgent.indexOf('Opera') >= 0){
+        alert('你是使用Opera')
+    }else if (navigator.userAgent.indexOf('Chrome') >= 0){
+        alert('你是使用Chrome')
+    }else{
+        alert('你是使用其他的浏览器浏览网页！')
+    }
+}
+
+/**
+ * 将URL转化为对象
+ * @param argu
+ * @returns {{}}
+ */
+function parseQueryString(argu){
+
+    var str = argu.split('?')[1];
+    var result = {};
+    var temp = str.split('&');
+    for(var i=0; i<temp.length; i++)
+    {
+        var temp2 = temp[i].split('=');
+        result[temp2[0]] = temp2[1];
+    }
+    return result;
+}
+
+/**
+ * 转换CSS中常用的颜色编码
+ * @param color
+ * @returns {string}
+ * @description         alert(toRGB("#0000FF"));          // 输出 rgb(0, 0, 255)
+                        alert(toRGB("invalid"));          // 输出 invalid
+                        alert(toRGB("#G00"));             // 输出 #G00
+ */
+function toRGB(color) {
+    var regex = /^#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})$/;
+    match = color.match(regex);
+
+    return match ? 'rgb('+parseInt(match[1], 16)+','+parseInt(match[2], 16)+','+parseInt(match[3], 16)+')' : color;
+}
+
+/**
+ * 去掉字符串首尾空格
+ * @returns {string}
+ */
+String.prototype.trim = function() {
+    return this.replace(/^\s+|\s+$/g, '');
+};
+
+/**
+ * 将下标index处的字符替换为repStr
+ * @param index
+ * @param repStr
+ * @returns {string}
+ */
+String.prototype.replaceByIndex = function(index, repStr) {
+    var prevStr = this.substring(0,index-1);
+    var nextStr = this.substring(index, this.length);
+    return prevStr+repStr+nextStr;
+};
+
+/**
+ * 二分查找
+ * @param value
+ */
+Array.prototype.binarySearch = function(value) {
+    var index = 0;
+    var find = false;
+    var left = 0;
+    var right= this.length;
+
+    while(left <= right)
+    {
+        var center = Math.floor((left+right)/2);
+        if(this[center] == value)
+        {
+            index = center;
+            find = true;
+        }
+        if(value < this[center])
+        {
+            right = center - 1;
+        }
+        else
+        {
+            left = center + 1;
+        }
+    }
+
+    if(find){
+        alert('要查找的元素下标是' + center);
+    }else{
+        alert('未找到');
+    }
+};
+
+/*
+ 请用CSS定义p标签，要求实现以下效果: 字体颜色在IE6下为黑色(#000000)；IE7下为红色(#ff0000)；而其他浏览器下为绿色(#00ff00)。
+ p{
+ color:#0f0;
+ _color:#000; //ie6
+ }
+ //ie7
+ *+html p{
+ color:#f00;
+ }
+ * */
+
+/*
+表格结构
+* <table align = "center" border="1" cellspacing = "0"cellpadding = "0" width = "50%">
+    <tr>
+        <th>表头</th>
+        <th>表头</th>
+    </tr>
+    <tr align = "center">
+        <td>中国</td>
+        <td>淘宝网</td>
+    </tr>
+ </table>
+
+ 列表结构
+ <dl>
+    <dt>定义列表中的项目</dt>
+    <dd>描述列表中的项目</dd>
+
+    <dt>定义列表中的项目</dt>
+    <dd>描述列表中的项目</dd>
+ </dl>
+
+ 表单结构
+ <form>
+    //<label> 标签的 for 属性应当与相关元素的 id 属性相同。
+    <label for="male">Male</label>
+    <input type="radio" name="sex" id="male" />
+ </form>
+* */
---
+++ 排序算法.js
@@ -0,0 +1,264 @@
+/*
+ 冒泡排序: 相邻排序，后项向前项冒泡。
+
+ 不带flag的冒泡排序时间复杂度:O(n2),
+ 带flag的冒泡排序时间复杂度:
+ 正序时(最好情况) 时间复杂度O(n)
+ 逆序时(最坏情况) 时间复杂度O(n2)
+ */
+/* 不带flag的冒泡排序 比较和交换次数都不少*/
+function bubbleSort_1(arr){
+
+    var i, j, temp;
+
+    for(i = 0; i < arr.length; i++){			/* 下标i从前向后 */
+        for(j = arr.length - 1; j >= i; j--){	/* 下标j从后向前 */
+
+            if(arr[j] > arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    alert(arr);
+}
+/* 带flag的冒泡排序   比较次数较少，交换次数不少*/
+function bubbleSort_2(arr){
+
+    var i, j, temp, flag = 0;	/* flag为0，不退出循环 */
+
+    for(i = 0; i < arr.length && !flag; i++){			/* 下标i从前向后 */
+        flag = 1;
+
+        for(j = arr.length -1; j >= i; j--){			/* 下标j从后向前 */
+            if(arr[j] > arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+
+                flag = 0;
+            }
+        }
+    }
+
+    alert(arr);
+}
+
+
+
+/*
+ 简单选择排序: 第一次从R[0]~R[n-1]中选取最小值，与R[0]交换，
+ 第二次从R[1]~R[n-1]中选取最小值，与R[1]交换，....，
+ 第i次从R[i-1]~R[n-1]中选取最小值，与R[i-1]交换，.....，
+ 第n-1次从R[n-2]~R[n-1]中选取最小值，与R[n-2]交换，
+ 总共通过n-1次，得到一个按排序码从小到大排列的有序序列
+
+ 特点：交换次数相当少，比较次数不少，时间复杂度: O(n2),性能上略优于不带flag的冒泡排序,简单选择排序是一种不稳定的排序方法
+ */
+function selectSort(arr){
+
+    var i, j, min, temp;
+
+    for(i = 0; i < arr.length; i++){
+        min = i;	/* 将当前下标定义为最小值下标 */
+
+        for(j = i+1; j <= arr.length; j++){
+            if(arr[min] > arr[j]){
+                min = j;
+            }
+        }
+
+        if(i != min){
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    alert(arr);
+}
+
+
+
+/*
+ 直接插入排序: 每次从无序表中取出第一个元素，把它插入到有序表的合适位置，使有序表仍然有序
+ 每步将一个待排序的记录按其关键字的大小插到前面已经排序的序列中的适当位置，直到全部记录插入完毕为止
+
+ 特点：时间复杂度: O(n2),性能上略优于冒泡排序，直接插入排序属于稳定的排序
+ */
+function insertSort(arr){
+
+    var i, j, temp;
+
+    for(i = 1; i < arr.length; i++){
+        if(arr[i] < arr[i-1]){							/* 判断后项是否小于前项 */
+            temp = arr[i];								/* 设置临时变量(相当于哨兵),将后项存入temp中 */
+            for(j = i-1; arr[j] > temp && j > -1; j--){	/* j为前项 *///i=1 j=0 temp=1
+                arr[j+1] = arr[j];						/* 后移 */
+            }
+            arr[j+1] = temp;								/* 插入至正确位置 */
+        }
+    }
+
+    alert(arr);
+}
+
+
+
+/*
+ 希尔排序: 是直接插入排序算法的一种更高效的改进版本，希尔排序是非稳定排序算法
+ 希尔排序是把记录按下标的一定增量分组，对每组使用直接插入排序算法排序；
+ 随着增量逐渐减少，每组包含的关键词越来越多，当增量减至1时，整个文件恰被分成一组，算法便终止
+
+ 特点：时间复杂度: 希尔排序的时间复杂度与增量序列的选取有关，例如希尔增量时间复杂度为O(n2)，
+ 而Hibbard增量的希尔排序的时间复杂度为O(n3/2)，希尔排序时间复杂度的下界是n(log2n)
+ */
+function shellSort(arr){
+
+    var increment = arr.length;
+    var i, j, temp;	/*temp哨兵*/
+
+    do{
+        increment = increment/3+1;	/* 增量序列 */
+
+        for(i = increment; i < arr.length; i++){
+            if(arr[i] < arr[i-increment]){
+                /* 需要将arr[i]插入有序增量子表 */
+
+                temp = arr[i];
+                for(j = i-increment; temp < arr[j] && j > -1; j -= increment){
+                    arr[j+increment] = arr[j];	/* 后移 */
+                }
+                arr[j+increment] = temp;	/* 插入 */
+            }
+        }
+    }while(increment > 2);
+
+    alert(arr);
+}
+
+
+
+/*
+ 归并排序: 将已有序的子序列合并，得到完全有序的序列；
+ 即先使每个子序列有序，再使子序列段间有序。
+ 若将两个有序表合并成一个有序表，称为二路归并
+
+ 特点：时间复杂度: O(nlogn),稳定的排序算法
+ */
+
+/*
+ 递归实现
+ */
+function mergeSort(arr){
+    msort(arr,arr,0,arr.length-1);
+
+    alert(arr);
+}
+
+/*
+ * 将SR[]归并为TR1[]
+ */
+function msort(sr, tr, start, end){
+    /* 这里的sr和tr1是数组指针，改变sr,tr就会改变L */
+
+    var mid;
+    var tempArr;
+
+    if(start == end){
+        tr[start] = sr[start];
+    }else{
+        mid = (start+end)/2;			/* 将sr[s..e]平分为sr[s...m]和sr[m+1...e] 这里分为了0~4和5~9*/
+        msort(sr, tempArr, start, mid);		/* 递归将sr[s...m]归并为有序的tempArr[s...m] */
+        msort(sr, tempArr, mid+1, end);		/* 递归将sr[m+1...e]归并为有序的tempArr[m+1...e] */
+
+        merge(tempArr, tr, start, mid, end);	/* 将tempArr[s...m]和tempArr[m+1...e]归并到tr[s...e] */
+    }
+}
+
+/*
+ * 将sr[start...mid]和sr[mid+1...end]归并为tr[start...end]
+ */
+function merge(sr, tr, start, mid, end){
+    var j, k, l;
+
+    for(j = mid+1, k = start; start <= mid && j <= end; k++){
+        if(sr[start] < sr[j]){
+            tr[k] = sr[start++];
+        }else{
+            tr[k] = sr[j++];
+        }
+    }
+
+    /* 将归并剩下的数组数据,移动到tr后面 */
+    if(start <= mid){
+        for(l = 0; l <= mid-start; l++){
+            tr[k+l] = sr[start+l];
+        }
+    }
+
+    if(j <= end){
+        for(l = 0; l <= end-j; l++){
+            tr[k+l] = sr[j+l];
+        }
+    }
+}
+
+
+
+
+/*
+ 快速排序: 通过一趟排序将要排序的数据分割成独立的两部分，
+ 其中一部分的所有数据都比另外一部分的所有数据都要小，
+ 然后再按此方法对这两部分数据分别进行快速排序，
+ 整个排序过程可以递归进行，以此达到整个数据变成有序序列
+
+ 特点：时间复杂度: O(nlogn),不稳定的排序算法
+ */
+
+/*
+ *	交换函数
+ */
+
+/* 快速排序 */
+function quickSort(arr){
+    QSort(arr, 0, arr.length-1);
+
+    alert(arr);
+}
+
+function QSort(arr, low, high){
+    var pivot;	/*中轴*/
+
+    if(low < high){
+        pivot = partition(arr, low, high);	/*将L均分为两部分*/
+
+        QSort(arr,low,pivot-1);	/*对低子表递归*/
+        QSort(arr,pivot+1,high);	/*对高子表递归*/
+    }
+}
+
+function partition(arr, low, high){
+    var pivotkey = arr[low];
+
+    while(low < high){
+        while(low < high && arr[high] >= pivotkey){
+            high--;
+        }
+
+        var temp = arr[low];
+        arr[low] = arr[high];
+        arr[high] = temp;	/* 将比中轴小的记录交换到低端 */
+
+        while(low < high && arr[low] <= pivotkey){
+            low++;
+        }
+        temp = arr[low];
+        arr[low] = arr[high];
+        arr[high] = temp;	/* 将比中轴大的记录交换到高端 */
+    }
+
+    return low;		/*返回中轴*/
+}
